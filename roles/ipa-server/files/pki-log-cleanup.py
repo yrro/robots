d27e38752f15116dc27815031243971effb59846
Sam Morris <sam@robots.org.uk>
2024-10-31 12:16:25 +0000

    ipa-server: rewrite pki-log-cleanup in Python

---
+++ roles/ipa-server/files/pki-log-cleanup.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+from datetime import date, datetime, timedelta
+from itertools import chain
+import os
+from pathlib import Path
+import sys
+
+from logging import basicConfig, getLogger
+
+
+logger = getLogger("pki-log-cleanup")
+
+
+def main():
+    basicConfig(level=os.environ.get("LOG_LEVEL", "INFO").upper(), format="%(message)s")
+
+    dry_run = int(os.environ.get("DRY_RUN", "1"))
+    if dry_run:
+        logger.info("This is a dry-run; no log files will be removed")
+
+    for path in get_paths("pki-tomcat"):
+        logger.info("%s %s", "Deleting" if not dry_run else "Not deleting", path)
+        if not dry_run:
+            path.unlink()
+
+
+def get_paths(instance):
+    for subsystem in ["acme", "ca", "kra", "pki"]:
+        log_dir = get_instance_subsystem_log_path(instance, subsystem)
+        datum = date.today()
+        yield from get_rlf_paths(log_dir, datum)
+        yield from get_debug_paths(log_dir, datum)
+
+
+def get_instance_subsystem_log_path(instance, subsystem):
+    assert len(instance) > 0
+    assert len(subsystem) > 0
+    return Path("/var/log/pki") / instance / subsystem
+
+
+def get_rlf_paths(log_dir, datum):
+    '''
+    com.netscape.cms.logging.RollingLogFile logs are rotated after 30 days or
+    2000 KiB. There is an 'expirationTime' property for each instance of
+    com.netscape.cms.logging.RollingLogFile in </etc/pki/pki-tomcat/*/CS.cfg>
+    but a comment in RollingLogFile.java says that it is not supported, and
+    there's no CS.cfg file for acma/pki subsystems so this property can't be
+    set for them anyway.
+    '''
+    rlf_retention = int(os.environ.get("RLF_RETAIN_DAYS", "14"))
+    assert rlf_retention > 0
+    cutoff = datum - timedelta(days=rlf_retention)
+
+    for glob in ["selftests.log.????-??-??.log", "system.????-??-??", "transactions.????-??-??"]:
+        for path in log_dir.glob(glob):
+            path_date = datetime.strptime(path.name[-14:-4], "%Y-%m-%d")
+            if path_date.date() < cutoff:
+                yield path
+
+    for path in log_dir.glob("signedAudit/*.??????????????"):
+        path_date = datetime.strptime(path.name[-14:-6], "%Y%m%d")
+        if path_date.date() < cutoff:
+            yield path
+
+
+def get_debug_paths(log_dir, datum):
+    '''
+    debug logs are rotated daily. According to
+    <https://github.com/dogtagpki/pki/wiki/Configuring-Subsystem-Debug-Log>
+    they are purged after 7 days, and even though maxDays is set to 7 in
+    </usr/share/pki/*/webapps/*/WEB-INF/classes/logging.properties>, they
+    aren't being purged on RHEL 8 or 9.
+    '''
+    debug_retention = int(os.environ.get("DEBUG_RETAIN_DAYS", "60"))
+    assert debug_retention > 0
+    cutoff = datum - timedelta(days=debug_retention)
+
+    for path in log_dir.glob("debug.????-??-??.log"):
+        path_date = datetime.strptime(path.name[6:16], "%Y-%m-%d")
+        if path_date.date() < cutoff:
+            yield path
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+# vim: ts=8 sts=4 sw=4 et
