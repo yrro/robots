765036949ad1258fc3e7ca2e8504eb3e45c8d81c
Sam Morris <sam@robots.org.uk>
2024-11-21 08:06:49 +0000

    typo

--- roles/ipa-server/files/ipa-pki-crl-updated.py
+++ roles/ipa-server/files/ipa-pki-crl-updated.py
@@ -7,8 +7,8 @@ from subprocess import run
 
 def main(argv):
     # The service will be started for every change to the crl directory;
-    # sleeping here during the 1st activation should prevent a additioal
-    # activation for subsequent changes during the duration of the sleep.
+    # sleeping here during the 1st activation should prevent additioal
+    # activations for subsequent changes during the duration of the sleep.
     time.sleep(5)
 
     run(['systemctl', 'try-restart', 'krb5kdc.service'], check=True)
