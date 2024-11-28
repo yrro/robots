3ff29bee4ffdec23cc078c0491b41c20c9caf881
Sam Morris <sam@robots.org.uk>
2024-11-28 13:33:21 +0000

    ipa-client: refactor metrics updates in crl fetching script

--- roles/ipa-server/files/ipa-pki-crl-fetch.py
+++ roles/ipa-server/files/ipa-pki-crl-fetch.py
@@ -41,9 +41,13 @@ def main(argv):
     signal.signal(signal.SIGTERM, handle_signal)
 
     server = ldap3.Server(os.environ["IPA_PKI_CRL_FETCH_LDAP_URL"], connect_timeout=6, tls=ldap3.Tls(validate=ssl.CERT_REQUIRED))
+
     conn = None
     sleep_for = timedelta(0)
     while not exit_.is_set():
+        if prom_dir:
+            update_metrics()
+
         logger.info("Next CRL update check in %r", sleep_for)
         exit_.wait(sleep_for.total_seconds())
 
@@ -75,9 +79,6 @@ def main(argv):
 
         sleep_for = max(timedelta(minutes=1, seconds=random.randint(-15, 14)), earliest_next_update_in/2)
 
-        if prom_file:
-            write_to_textfile(prom_file, prom_reg)
-
     return 0
 
 
@@ -101,19 +102,28 @@ def process_crl(crl):
     new_crl = x509.load_der_x509_crl(crl.certificateRevocationList.value)
     new_crl_number = new_crl.extensions.get_extension_for_oid(x509.CRLNumber.oid).value.crl_number
 
-    if old_crl_number == new_crl_number:
-        logger.info("CRL %r %d has no updates", crl.cn.value, old_crl_number)
-        metric_crl = old_crl
-    else:
+    if old_crl_number != new_crl_number:
         logger.info("CRL %r updated %r → %r", crl.cn.value, old_crl_number, new_crl_number)
         crl_file.write_bytes(new_crl.public_bytes(serialization.Encoding.PEM))
-        metric_crl = new_crl
+    else:
+        logger.info("CRL %r %d has no updates", crl.cn.value, old_crl_number)
 
-    prom_crl_last_update.labels(crl_file, metric_crl.issuer.rfc4514_string()).set(metric_crl.last_update.replace(tzinfo=timezone.utc).timestamp())
-    prom_crl_next_update.labels(crl_file, metric_crl.issuer.rfc4514_string()).set(metric_crl.next_update.replace(tzinfo=timezone.utc).timestamp())
-    prom_crl_number.labels(crl_file, metric_crl.issuer.rfc4514_string()).set(metric_crl)
+
+def update_metrics():
+    for crl_file in crl_dir.glob("*.crl"):
+        try:
+            crl = x509.load_pem_x509_crl(crl_file.read_bytes())
+        except:
+            logger.exception("Unable to read CRL from %r", crl_file)
+            continue
+
+        labelvalues = crl_file, crl.issuer.rfc4514_string()
+        prom_crl_last_update.labels(*labelvalues).set(crl.last_update.replace(tzinfo=timezone.utc).timestamp())
+        prom_crl_next_update.labels(*labelvalues).set(crl.next_update.replace(tzinfo=timezone.utc).timestamp())
+        prom_crl_number.labels(*labelvalues).set(crl.extensions.get_extension_for_oid(x509.CRLNumber.oid).value.crl_number)
+
     prom_status.set_to_current_time()
-
+    write_to_textfile(prom_file, prom_reg)
 
 
 if __name__ == "__main__":
