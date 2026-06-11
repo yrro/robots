285760a6427b4d21656638c779bcfea7b2027330
Sam Morris <sam@robots.org.uk>
2026-06-11 20:36:50 +0100

    feat(mailserver): get TLS server certificates for imap, smtp and mta-sts via httpd mod_md

---
+++ roles/mailserver/files/httpd/md-challenge-dns.py
@@ -0,0 +1,188 @@
+#!/usr/bin/python -P
+
+# DNS updates are possible because of this update policy in the zone:
+#
+#   grant EXAMPLE.COM krb5-subdomain acme-challenge.example.com. TXT;
+#
+# ... which means "any principal in EXAMPLE.COM with a primary of 'host' may
+# update resource records beneath beneath acme-challenge.example.com if the
+# type is TXT"; oops!
+#
+# The best we can do is:
+#
+#   grant EXAMPLE.COM krb5-selfsub . TXT;
+#
+# ... which means "any principal in EXAMPLE.COM with a primary of 'host' may
+# update resource records beneath its own name if the type is TXT".
+#
+# Sadly there's no way to combine the behaviour so that we can
+# grant the ability to update TXT records beneath a particular domain name to a
+# particular principal. :(
+#
+# Then a special host principal for the mailserver host was created with:
+#
+#   # ipa host-add --force mailserver.acme-challenge.example.com
+#
+# To allow the mailserver host to manage the special host's Kerberos keys
+# (i.e., obtain a keytab for it), the mailserver is added as a manager of the
+# special host:
+#
+#   # ipa host-add-managedby mailserver.acme-challenge.example.com --hosts=mailserver.example.com
+#
+# GSS-TSIG code adapted from
+# <https://stackoverflow.com/questions/55900836/generate-tsig-keyring-as-encoded-byte-string-for-dns-update>
+
+import logging
+import logging.handlers
+import os
+import socket
+import sys
+import time
+import uuid
+
+import dns.rdtypes.ANY.TKEY
+import dns.resolver
+import dns.tsig
+import dns.update
+import gssapi
+
+
+logger = logging.getLogger("md-challenge-dns")
+
+
+def main(argv):
+    os.environ["GSS_USE_PROXY"] = "1"
+    os.environ["GSSPROXY_SOCKET"] = "/var/lib/gssproxy/md-challenge.sock"
+
+    if len(argv[1:]) == 3:
+        command, domain, token = argv[1:4]
+    elif len(argv[1:]) == 2:
+        command, domain = argv[1:3]
+        token = None
+    else:
+        logger.error("expected 2 or 3 arguments; got %r", argv[1:])
+        return 1
+    
+    target = resolve_target(dns.name.from_text(domain))
+    target_zone = dns.resolver.zone_for_name(target)
+    target_relative = target.relativize(target_zone)
+
+    target_zone_soa = dns.resolver.resolve(target_zone, dns.rdatatype.SOA)
+    target_zone_mname = target_zone_soa.rrset[0].mname
+
+    update = dns.update.Update(target_zone)
+    match command:
+        case "setup":
+            update.add(target_relative, 10, dns.rdatatype.TXT, token)
+        case "teardown":
+            update.delete(target_relative, dns.rdatatype.TXT)
+        case _:
+            logger.error("Unknown command %r", command)
+            return 1
+
+    target_server = dns.resolver.resolve_name(target_zone_mname)
+    success = False
+    excs = []
+    for addr in target_server.addresses():
+        log_msg = "operation=%s opcode=%s rr=%s zone=%s master=%s address=%s"
+        log_args = [command, dns.opcode.to_text(update.opcode()), target_relative, target_zone, target_zone_mname, addr]
+        try:
+            key_name, key_ring = gss_tsig_negotiate(target_zone_mname, addr, None)
+            update.use_tsig(keyring=key_ring, keyname=key_name, algorithm=dns.tsig.GSS_TSIG)
+            response = dns.query.tcp(update, addr, timeout=5)
+            log_msg += " rcode=%s"
+            log_args.append(dns.rcode.to_text(response.rcode()))
+            if response.rcode() == dns.rcode.NOERROR:
+                excs.clear()
+                success = True
+                break
+        except Exception as e:
+            log_msg += " error=%r"
+            log_args.append(str(e))
+            excs.append(e)
+            continue
+        finally:
+            logger.info(log_msg, *log_args)
+
+    if excs:
+        raise ExceptionGroup("All DNS update operations failed", excs)
+
+    if not success:
+        return 1
+
+    if command == "setup":
+        # XXX poll all nameservers until the recors is available?
+        time.sleep(5)
+
+    return 0
+
+
+def resolve_target(name):
+    challenge_name = dns.name.from_text(f"_acme-challenge", origin=None) + name
+    try:
+        ans = dns.resolver.resolve(challenge_name, dns.rdatatype.TXT)
+    except dns.resolver.NoAnswer as e:
+        return e.canonical_name()
+    except dns.resolver.NXDOMAIN as e:
+        return e.canonical_name
+    else:
+        return ans.canonical_name
+
+
+def gss_tsig_negotiate(server_name, server_addr, creds=None):
+    # Acquire GSSAPI credentials
+    gss_name = gssapi.Name(f"DNS@{server_name}", gssapi.NameType.hostbased_service)
+    gss_ctx = gssapi.SecurityContext(name=gss_name, creds=creds, usage="initiate")
+
+    # RFC 2930 suggests key names use the server name in their most significant
+    # part
+    keyname = dns.name.from_text(f"{uuid.uuid4()}.{server_name}")
+    key = dns.tsig.Key(keyname, gss_ctx, dns.tsig.GSS_TSIG)
+    keyring = dns.tsig.GSSTSigAdapter({keyname: key})
+
+    token = gss_ctx.step()
+    while not gss_ctx.complete:
+        tkey_query = build_tkey_query(token, keyring, keyname)
+        response = dns.query.tcp(tkey_query, server_addr, timeout=5)
+        # Original comment
+        # <https://github.com/rthalley/dnspython/pull/530#issuecomment-658959755>:
+        # "this if statement is a bit redundant, but if the final token comes
+        # back with TSIG attached then the patch to message.py will
+        # automatically step the security context. We dont want to excessively
+        # step the context."
+        if not gss_ctx.complete:
+            token = gss_ctx.step(response.answer[0][0].key)
+
+    return keyname, keyring
+
+
+def build_tkey_query(token, keyring, keyname):
+    from dns.rdtypes.ANY.TKEY import TKEY
+    datum = int(time.time())
+    tkey = TKEY(rdclass=dns.rdataclass.ANY, rdtype=dns.rdatatype.TKEY, algorithm=dns.tsig.GSS_TSIG, inception=datum, expiration=datum, mode=TKEY.GSSAPI_NEGOTIATION, error=dns.rcode.NOERROR, key=token)
+    query = dns.message.make_query(keyname, dns.rdatatype.TKEY, dns.rdataclass.ANY)
+    query.keyring = keyring
+    query.find_rrset(dns.message.ADDITIONAL, keyname, dns.rdataclass.ANY, dns.rdatatype.TKEY, create=True).add(tkey)
+    return query
+
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+    logger.critical("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+
+if __name__ == "__main__":
+    log_config = {}
+    if not sys.stdin:
+        # mod_md discards what we write to only logs our stderr if its LogLevel
+        # is set to debug.  SELinux policy prevents httpd_t writing to syslog.
+        # So boring old log files it will have to be.
+        log_config["handlers"] = [logging.handlers.RotatingFileHandler("/var/log/md-challenge-dns/md-challenge-dns.log", maxBytes=2**20, backupCount=2)]
+        log_config["format"] = "%(asctime)s %(levelname)s:%(name)s %(message)s"
+    logging.basicConfig(level="INFO", **log_config)
+    sys.excepthook = handle_exception
+    sys.exit(main(sys.argv))
+
+# vim: ts=8 sts=4 sw=4 et
