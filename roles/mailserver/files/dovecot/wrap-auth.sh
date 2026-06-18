ec6f358fa3567288ddb41ae499d7d37b725d3de0
Sam Morris <sam@robots.org.uk>
2026-06-18 11:44:20 +0100

    feat(dovecot) use gss-proxy
    The environment variables were not making it through to the auth process
    because it is launched with the AT_SECURE auxv flag set. This is done by
    SELinux because the auth process transitions to a different domain.
    
    The wrapper script would have to clean up any possible attack vectors
    tat the parent process might have. For example, it has to sanitize PATH.

---
+++ roles/mailserver/files/dovecot/wrap-auth.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eEuo pipefail
+
+# This wrapper script is launched with AT_SECURE due to the domain transition
+# from dovecot_t → dovecot_auth_t.
+
+# Audit the original environment
+(echo "Launching with environment:"; /usr/bin/printenv) | /usr/bin/logger -t wrap-auth
+
+# Sanitize the environment. ld-linux has already removed LD_PRELOAD and
+# friends.
+export PATH=/usr/local/bin:/usr/bin
+
+exec /usr/libexec/dovecot/auth "$@"
