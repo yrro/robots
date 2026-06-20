f659591bf66ed2b69105d629ac376eee22b548e3
Sam Morris <sam@robots.org.uk>
2026-06-20 10:35:42 +0100

    feat(mailserver): enable spam/ham reporting to rspamd

--- roles/mailserver/files/dovecot/rspamd-report.sh
+++ roles/mailserver/files/dovecot/rspamd-report.sh
@@ -9,13 +9,11 @@ case "$class" in
 spam|ham)
     ;;
 *)
-    printf "NOT reporting class=%q user=%q" "$class" "$user" | logger -p maill.err -t rspamd-report
+    printf "class=%q user=%q action=NONE" "$class" "$user" | logger -p maill.err -t rspamd-report
     exit 0
 esac
 
-printf "reporting class=%q user=%q" "$class" "$user" | logger -p mail.info -t rspamd-report
-# connects to controller (port 11334)
-#exec /usr/bin/rspamc -h -P secret "learn_${class}"
-exit 0
+printf "class=%q user=%q action=REPORT" "$class" "$user" | logger -p mail.info -t rspamd-report
+exec /usr/bin/rspamc -h localhost "learn_${class}"
 
 # vim: ts=8 sts=4 sw=4 et
